8f11eeebad5b33f1ffcd0c86f122c2249ab6724d
tim <timlieng1094@gmail.com>
2018-02-02 00:26:58 +0000

    form can now handle title, author and description

--- quiz-app/src/components/create-quiz-form/CreateQuizForm.js
+++ quiz-app/src/components/create-quiz-form/CreateQuizForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import EditableList from './EditableList';
 
 class CreateQuizForm extends React.Component {
     constructor(props) {
@@ -12,20 +13,82 @@ class CreateQuizForm extends React.Component {
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        console.log(this.state);
     }
 
     handleChange(event) {
+        let state = {...this.state};
+
+        if (event.target.id === "title-input") {
+            this.setState({title: event.target.value})
+        }
+        if (event.target.id === "author-input") {
+            this.setState({author: event.target.value})
+        }
+        if (event.target.id === "description-textarea") {
+            this.setState({description: event.target.value})
+        }
+        if (event.target.className === "question-input") {
+            state.questions[0].question = event.target.value;  //need to change correct index
+            this.setState({questions: state.questions})
+        }
+        else {
+            console.log("woops didnt handle change");
+        }
+    }
+
+    handleClick(event) {
+        let state = {...this.state};
+
+        if (event.target.className === "addItemButton") {
+            this.setState({questions: state.questions.concat({question: null, answers: []})})
+        }
+        if (event.target.className === "removeItemButton") {    //doesnt work
+            this.setState({questions: state.questions.splice(0,1)})
+        }
+    }
+
+
+    renderQuestions() {
+        return (
+            this.state.questions.map((question,index)=>(<Question question={question} id={index} onChange={this.handleChange}/>))
+        )
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSumbit}>
-
+            <form onSubmit={this.handleSubmit}>
+              <div id="title-wrapper">
+                <label id="title-label">Title: </label>
+                <input id="title-input" name="title" type="text" onChange={this.handleChange} ></input>
+              </div>  
+              <div id="author-wrapper">
+                <label id="author-wrapper">Author: </label>
+                <input id="author-input" name="author" type="text" onChange={this.handleChange} ></input>
+              </div> 
+              <div id="description-wrapper">
+                <label id="description-label">Description: </label>
+                <textarea id="description-textarea" name="description" onChange={this.handleChange} ></textarea>
+              </div>
+              <EditableList onClick={this.handleClick} components={this.renderQuestions()} />
+              <button id="submit-button" type="submit">Submit</button>
             </form>
+        )
+    }
+}
+
+class Question extends React.Component {
+
+    render() {
+        return (
+            <div className="question-wrapper" id={this.props.id}>
+              <input className="question-input" id={this.props.id} name="question" type="text" onChange={this.props.onChange}></input>
+            </div>
         )
     }
 }
