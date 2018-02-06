4bfee3a2d3bf74f5bd535f67f4e86bf4eb393726
tim <timlieng1094@gmail.com>
2018-02-06 11:52:13 +0000

    fixed bugs on quiz-form

--- quiz-app/src/components/quiz-form.js
+++ quiz-app/src/components/quiz-form.js
@@ -8,22 +8,24 @@ QuizForm.propTypes = {
     onTitleChange: PropTypes.func.isRequired,
 }
 
-const QuizForm = ({onAddQuestion, onAuthorChange, onSubmit, onTitleChange}) => {
-    <form className="create-quiz-form" onSubmit={onSubmit}>
-      <div className="title-wrapper">
-        <label className="title-label">Title: </label>
-        <input className="title-input" type="text" onChange={onTitleChange} ></input>
-      </div>  
-      <div className="author-wrapper">
-        <label className="author-wrapper">Author: </label>
-        <input className="author-input" type="text" onChange={onAuthorChange} ></input>
-      </div> 
-      <div className="question-list-wrapper">
-      
-        <button className="add-question-button" onClick={onAddQuestion} type="button">add question</button>
-      </div>
-      <button className="submit-button" type="submit">Submit</button>
-    </form>
+function QuizForm({onAddQuestion, onAuthorChange, onSubmit, onTitleChange}) { 
+    return (
+        <form className="create-quiz-form" onSubmit={onSubmit}>
+          <div className="title-wrapper">
+            <label className="title-label">Title: </label>
+            <input className="title-input" type="text" onChange={onTitleChange} ></input>
+          </div>  
+          <div className="author-wrapper">
+            <label className="author-wrapper">Author: </label>
+            <input className="author-input" type="text" onChange={onAuthorChange} ></input>
+          </div> 
+          <div className="question-list-wrapper">
+        
+            <button className="add-question-button" onClick={onAddQuestion} type="button">add question</button>
+          </div>
+          <button className="submit-button" type="submit">Submit</button>
+        </form>
+    )
 }
 
 export default QuizForm
