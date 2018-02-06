ff498dc01cf3219e889b41c7228005dd0c71ec5e
tim <timlieng1094@gmail.com>
2018-02-06 11:42:51 +0000

    converted quiz-form to functional presentational component

--- quiz-app/src/components/quiz-form.js
+++ quiz-app/src/components/quiz-form.js
@@ -1,50 +1,29 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
 
-import { 
-    setAuthor,
-    setTitle,  
-    } from '../actions';
-
-class QuizForm extends React.Component {
-
-    handleAuthorChange = (event) => {
-        this.props.setAuthor(event.target.value);
-    }
-
-    handleTitleChange = (event) => {
-        this.props.setTitle(event.target.value);
-    }
-
-    render() {
-        return (
-          <form className="create-quiz-form" onSubmit={this.handleSubmit}>
-            <div className="title-wrapper">
-              <label className="title-label">Title: </label>
-              <input className="title-input" type="text" onChange={this.handleTitleChange} ></input>
-            </div>  
-            <div className="author-wrapper">
-              <label className="author-wrapper">Author: </label>
-              <input className="author-input" type="text" onChange={this.handleAuthorChange} ></input>
-            </div> 
-            <div className="question-list-wrapper">
-              
-              <button className="add-question-button" onClick={this.handleClick }>add question</button>
-            </div>
-            <button className="submit-button" type="submit">Submit</button>
-          </form>
-        )
-    }
+QuizForm.propTypes = {
+    onAddQuestion: PropTypes.func.isRequired,
+    onAuthorChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    onTitleChange: PropTypes.func.isRequired,
 }
 
-const mapStateToProps = (state) => ({
-})
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    setAuthor: (author) => {dispatch(setAuthor(author))},
-    setTitle: (title) => {dispatch(setTitle(title))},
-})
-
-QuizForm = connect(mapStateToProps,mapDispatchToProps)(QuizForm)
+const QuizForm = ({onAddQuestion, onAuthorChange, onSubmit, onTitleChange}) => {
+    <form className="create-quiz-form" onSubmit={props.onSubmit}>
+      <div className="title-wrapper">
+        <label className="title-label">Title: </label>
+        <input className="title-input" type="text" onChange={this.handleTitleChange} ></input>
+      </div>  
+      <div className="author-wrapper">
+        <label className="author-wrapper">Author: </label>
+        <input className="author-input" type="text" onChange={this.handleAuthorChange} ></input>
+      </div> 
+      <div className="question-list-wrapper">
+      
+        <button className="add-question-button" onClick={this.handleClick} type="button">add question</button>
+      </div>
+      <button className="submit-button" type="submit">Submit</button>
+    </form>
+}
 
 export default QuizForm
