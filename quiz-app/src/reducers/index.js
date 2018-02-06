efa6de8ff1f3893928fe1547e63dea939c03c72f
tim <timlieng1094@gmail.com>
2018-02-06 00:11:37 +0000

    added add_question reducer function

--- quiz-app/src/reducers/index.js
+++ quiz-app/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 
 import author from './author'
 import description from './description'
+import questionList from './questionList'
 import title from './title'
 
 const reducer = combineReducers({
   author,
   description,
+  questionList,
   title,
 })
 
