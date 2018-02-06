7c70c100ca1ed53f6f841d665c5e31110d6ffa91
tim <timlieng1094@gmail.com>
2018-02-06 11:45:47 +0000

    fixed bugs on quiz-form

--- quiz-app/src/components/quiz-form.js
+++ quiz-app/src/components/quiz-form.js
@@ -9,18 +9,18 @@ QuizForm.propTypes = {
 }
 
 const QuizForm = ({onAddQuestion, onAuthorChange, onSubmit, onTitleChange}) => {
-    <form className="create-quiz-form" onSubmit={props.onSubmit}>
+    <form className="create-quiz-form" onSubmit={onSubmit}>
       <div className="title-wrapper">
         <label className="title-label">Title: </label>
-        <input className="title-input" type="text" onChange={this.handleTitleChange} ></input>
+        <input className="title-input" type="text" onChange={onTitleChange} ></input>
       </div>  
       <div className="author-wrapper">
         <label className="author-wrapper">Author: </label>
-        <input className="author-input" type="text" onChange={this.handleAuthorChange} ></input>
+        <input className="author-input" type="text" onChange={onAuthorChange} ></input>
       </div> 
       <div className="question-list-wrapper">
       
-        <button className="add-question-button" onClick={this.handleClick} type="button">add question</button>
+        <button className="add-question-button" onClick={onAddQuestion} type="button">add question</button>
       </div>
       <button className="submit-button" type="submit">Submit</button>
     </form>
