9f052951194c5370a3fd77d8421b10bbb2712374
tim <timlieng1094@gmail.com>
2018-02-06 00:12:03 +0000

    added add_question reducer function

---
+++ quiz-app/src/reducers/title.js
@@ -0,0 +1,10 @@
+const title = (state = "", action) => {
+    switch (action.type) {
+        case "SET_TITLE":
+            return action.title
+        default:
+            return state
+    }
+}
+
+export default title
