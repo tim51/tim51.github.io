1103025d296b60612afcf799fbbbc4feb9df6238
tim <timlieng1094@gmail.com>
2018-02-06 00:03:42 +0000

    added set title reducer

--- quiz-app/src/reducers/index.js
+++ quiz-app/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 
 import author from './author'
 import description from './description'
+import title from './title'
 
 const reducer = combineReducers({
   author,
   description,
+  title,
 })
 
 export default reducer
