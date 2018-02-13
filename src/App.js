e10c217c664af4ee889caa837cdbbe2089fdb546
tim <timlieng1094@gmail.com>
2018-02-13 16:15:40 +0000

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div id="root"></div>
         hello
       </div>
     );
